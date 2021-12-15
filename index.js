e0dc7a2416d16195c9ee2fcc75fd8d1571cfbf61
Ricardo Díaz <rddiazolivos@gmail.com>
2021-12-15 16:53:02 -0300

    añado cors

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
 const pokemonRoutes = require('./routes/index')
 const port = 3000
 
+app.use(cors())
 app.get('/', (req, res) => res.status(200).send({
   'list pokemons': `${process.env.HOST}/pokemons`,
   'get pokemon': `${process.env.HOST}/pokemons/:id`
 }))
 app.use('/pokemons', pokemonRoutes)
+
 const server  = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = {app, server}
