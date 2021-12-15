82a541894179b17ba390f0417c54ac4019fe62d1
Ricardo Díaz <rddiazolivos@gmail.com>
2021-12-15 14:09:20 -0300

    quito log

--- redis.js
+++ redis.js
@@ -1,9 +1,9 @@
 const redis = require('redis')
 const redisPort = 6379
 const client = redis.createClient(redisPort);
-client.on("error", (err) => {
-  console.log(err);
-});
+// client.on("error", (err) => {
+//   console.log(err);
+// });
 client.on('connect', () => {
   console.log(`Redis connected on port`);
 });
